7a941f2d4ff427e27bf0ab311ba0d09f5c141464
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-28 08:32:05 +0530

    first

---
+++ apply-credit-card-service/src/main/java/com/learn/apply_credit_card_service/ApplyCreditCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learn.apply_credit_card_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyCreditCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyCreditCardServiceApplication.class, args);
+	}
+
+}
---
+++ apply-credit-card-service/src/test/java/com/learn/apply_credit_card_service/ApplyCreditCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.apply_credit_card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyCreditCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
