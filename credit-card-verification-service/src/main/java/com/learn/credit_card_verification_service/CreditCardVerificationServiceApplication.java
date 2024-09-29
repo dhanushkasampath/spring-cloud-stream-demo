b3b21d246deb45c729bb6a5085c7d889f7af9166
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-29 06:56:40 +0530

    verification service added

---
+++ credit-card-verification-service/src/main/java/com/learn/credit_card_verification_service/CreditCardVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learn.credit_card_verification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardVerificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardVerificationServiceApplication.class, args);
+	}
+
+}
